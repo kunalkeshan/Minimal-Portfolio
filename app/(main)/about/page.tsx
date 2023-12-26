00d28ba97808cfb8f565864294f3b298f27a932e
Kunal Keshan <kunalkeshan12@gmail.com>
2023-12-27 00:29:24 +0530

    chore: added about hero section
    - closes #31

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Body from '@/components/about/Body';
+import Hero from '@/components/about/Hero';
+import { sanityFetch } from '@/sanity/lib/sanityFetch';
+import { mainPortfolioUserQuery } from '@/sanity/lib/queries';
 
-const AboutPage = () => {
-	return <div>AboutPage</div>;
+const AboutPage = async () => {
+	const user = await sanityFetch<Author>({ query: mainPortfolioUserQuery });
+	return (
+		<main className='p-4 w-full max-w-4xl mx-auto'>
+			<Hero user={user} />
+			<Body />
+		</main>
+	);
 };
 
 export default AboutPage;
--- components/about/Hero.tsx
+++ components/about/Hero.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import Image from 'next/image';
 
-const Hero = () => {
-	return <div>Hero</div>;
+type HeroProps = React.ComponentProps<'section'> & {
+	user: Author;
+};
+
+const Hero: React.FC<HeroProps> = ({ user }) => {
+	return (
+		<section className='w-full grid grid-cols-1 md:grid-cols-2 gap-8'>
+			<div>
+				<h1 className='text-2xl'>
+					{user?.name ? `I'm ${user.name}` : 'Minimal Portfolio'}
+				</h1>
+				<h2 className='text-lg mt-4'>
+					{user?.about ??
+						'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nemo nobis quam assumenda. Illum repellendus veritatis vero, blanditiis accusantium nesciunt?'}
+				</h2>
+			</div>
+			<div className='flex items-center justify-center'>
+				<div className='max-w-xs'>
+					<Image
+						src='/svgs/about.svg'
+						width={100}
+						height={100}
+						className='w-full h-auto object-cover'
+						alt=''
+					/>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Hero;
