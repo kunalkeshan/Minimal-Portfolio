4c0eafd5f1569a1bf06480e54cb99a49a2a2ae10
Kunal Keshan <kunalkeshan12@gmail.com>
2023-12-27 23:23:00 +0530

    chore: home hero (wip)

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,12 @@
+import Hero from '@/components/home/Hero';
 import { mainPortfolioUserQuery } from '@/sanity/lib/queries';
 import { sanityFetch } from '@/sanity/lib/sanityFetch';
 
 export default async function Home() {
 	const user = await sanityFetch<Author>({ query: mainPortfolioUserQuery });
-	return <main>Home</main>;
+	return (
+		<main className='p-4 w-full max-w-4xl mx-auto'>
+			<Hero user={user} />
+		</main>
+	);
 }
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { ArrowRight } from 'lucide-react';
 
-const Hero = () => {
-	return <section className='max-w-4xl 2xl:max-w-7xl mx-auto'>Hero</section>;
+type HomeProps = React.ComponentProps<'section'> & {
+	user: Author;
+};
+
+const Hero: React.FC<HomeProps> = ({ user }) => {
+	return (
+		<section className=''>
+			<h1 className='text-2xl'>
+				{user?.name ? `Hi! I'm ${user.name}` : 'Minimal Portfolio'}
+			</h1>
+			<h2 className='text-lg mt-4'>
+				{user?.about ??
+					'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nemo nobis quam assumenda. Illum repellendus veritatis vero, blanditiis accusantium nesciunt?'}
+			</h2>
+			<Link
+				href='/about'
+				className='underline underline-offset-2 flex items-center gap-2'
+			>
+				<span>Learn more about me</span>
+				<ArrowRight strokeWidth={1.5} size={20} />
+			</Link>
+		</section>
+	);
 };
 
 export default Hero;
